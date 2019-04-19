acd904c5bb26ecd96c8c0cb00147910a2da3af48
Tgleason1980 <tglabon@gmail.com>
2019-04-18 21:48:15 -0500

    3 events made

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+const btn = document.querySelectorAll(".btn")[1];
+btn.addEventListener('mouseover', function (event) {
+    btn.style.backgroundColor = "purple";
+});
+
+const noFoot = document.querySelector(".footer");
+window.addEventListener('keydown', function (event) {
+    noFoot.style.display = "none";
+});
+
+const pickColor = document.querySelector(".content-pick");
+window.addEventListener('wheel', function (event) {
+    pickColor.style.color = "blue";
+});
+
